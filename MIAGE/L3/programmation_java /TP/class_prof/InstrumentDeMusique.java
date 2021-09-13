8391f5726f3838f53dce840a77c64305268eaf1f
Thomas LEBRET <42217466+ThomasLEBRET@users.noreply.github.com>
2021-09-13 10:37:51 +0200

    TP1 fin

--- MIAGE/L3/programmation_java /TP/class_prof/InstrumentDeMusique.java
+++
@@ -1,8 +0,0 @@
-package musiqueTD1Feuilles20212022ADiffuser;
-
-public interface InstrumentDeMusique {
-	public String jouer();
-	public String indiquer_identite();
-	public String indiquer_proprietaire();
-	public void changerProprietaire(String nouveau);
-}
